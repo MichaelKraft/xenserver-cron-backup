d84dcbb24b3c039a8987ee435ed7bb4861d8b2d7
Michael Kraft <michael.kraft@sparknet.net>
2017-02-02 14:38:57 -0600

    first revision

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # xenserver-cron-backup
-Bash script that can backup VM's from a XenServer.
+Bash script for backing up VM's from a XenServer.
+
+* Tested on a single-host personal environment.
+* Designed to be run as a cron by the Xen hosts' root user.
+
+#### Settings
+
+* `BACKUP_USING_NAMES` If set to 1 will save the backups as the Name of the virtual machine. If 0 will save as the machine's UUID.
+* `MAX_WAIT_TIME` Number of seconds the script will wait to verify the machine is in the halted/running state before and after backup.
+* `BACKUPROOT` Directory in which to deposit .xva files.
+* `machine_uuids` Array of UUID's targeting the Virtual Machines to be backed up. Use `xe vm-list` to see them.
+
+#### To Do
+
+- [ ] Add a setting to indicate number of backups kept (currently locked at 3 per VM)
+- [ ] Include output able to be parsed by a Nagios check.
+
+#### Copyright (c) 2015 Michael Kraft
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ vm-backup.sh
@@ -0,0 +1,67 @@
+#/bin/bash
+
+#Settings
+BACKUP_USING_NAMES=1
+MAX_WAIT_TIME=100
+BACKUPROOT="/mnt/vmbackup/"
+
+declare -a machine_uuids=("00000000-0000-0000-0000-000000000000",
+                          "00000000-0000-0000-0000-000000000000")
+
+#Backup Script
+for machine_uuid in "${machine_uuids[@]}"
+do
+        if [ $BACKUP_USING_NAMES == 1 ]
+        then
+            NAME=`xe vm-param-get param-name=name-label uuid=$machine_uuid`
+        else
+            NAME=$machine_uuid
+        fi
+
+        echo "Stopping $NAME..."
+        xe vm-shutdown uuid=$machine_uuid
+
+        TIMEOUT=0
+        STATUS="running"
+        while [ $STATUS != "halted" ]; do
+            sleep 1
+            STATUS=`xe vm-param-get param-name=power-state uuid=$machine_uuid`
+            TIMEOUT=$((TIMEOUT+1))
+            echo "TEST: $STATUS"
+            if [ $TIMEOUT -gt $MAX_WAIT_TIME ] 
+            then
+                echo "Backup failed!"
+                echo "Could not stop VM: $NAME"
+                echo "ID: $machine_uuid"
+                exit 1
+            fi
+        done
+
+        echo "Stopped."
+        date
+
+        rm $BACKUPROOT$machine.xva.1
+        mv $BACKUPROOT$machine.xva.0 $BACKUPROOT$machine.xva.1
+        mv $BACKUPROOT$machine.xva $BACKUPROOT$machine.xva.0
+        
+        xe vm-export vm=$machine filename=$BACKUPROOT$NAME.xva
+
+        echo "Completed backup of $NAME."
+        date
+
+        xe vm-start uuid=$machine_uuid
+        STATUS="halted"
+        TIMEOUT=0
+        while [ $STATUS != "running" ]; do
+            sleep 1
+            STATUS=`xe vm-param-get param-name=power-state uuid=$machine_uuid`
+            TIMEOUT=$((TIMEOUT+1))
+            if [ $TIMEOUT -gt $MAX_WAIT_TIME ] 
+            then
+                echo "Backup succeeded!"
+                echo "Could not start VM: $NAME"
+                echo "ID: $machine_uuid"
+            fi
+        done
+        echo "$NAME started."
+done
